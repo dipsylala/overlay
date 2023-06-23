a65beb5a632bd3f41700cd40648e7503469d7f12
Marcus Watson <mwatson@veracode.com>
2023-06-23 13:55:43 +0100

    updating veracode tests

--- src/background/advisory/veracode.test.js
+++ src/background/advisory/veracode.test.js
@@ -1,37 +1,15 @@
 import { describe, expect, test } from '@jest/globals';
-import snyk from './veracode.js';
+import veracode from './veracode.js';
 
 describe('veracode', () => {
   test('should return details for package', async () => {
-    const res = await snyk({ type: 'npm', name: 'react' });
+    const res = await veracode({ type: 'npm', name: 'react' });
 
     expect(res).toStrictEqual({
       issues: expect.any(Number),
       reportUrl: expect.any(String),
       summary: expect.any(String),
-      data: {
-        score: expect.any(Number),
-        maxScore: 100,
-        level: expect.stringMatching(/^GOOD|WARNING|BAD$/),
-        badges: {
-          security: {
-            level: expect.stringMatching(/^GOOD|WARNING|BAD$/),
-            description: 'No known security issues',
-          },
-          popularity: {
-            level: expect.stringMatching(/^GOOD|WARNING|BAD$/),
-            description: 'Key ecosystem project',
-          },
-          maintenance: {
-            level: expect.stringMatching(/^GOOD|WARNING|BAD$/),
-            description: 'Sustainable',
-          },
-          community: {
-            level: expect.stringMatching(/^GOOD|WARNING|BAD$/),
-            description: 'Active',
-          },
-        },
-      },
+      data: {},
     });
   });
 });
